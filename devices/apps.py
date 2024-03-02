4b48ac7d65daa47cc2ba86f817e9cef246b8f6de
leo98ber <leonardoberoes94@gmail.com>
2024-03-02 11:14:17 -0400

    feat: add python packages in device
    BREAKING CHANGE: These folders are the module device containers

---
+++ devices/apps.py
@@ -0,0 +1,10 @@
+#Django
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class DeviceAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'devices'
+    verbose_name = _('Devices')
+
